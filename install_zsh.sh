8c7ccc2efbbe0916387efc0b8202eaa3607dd7b9
root <root@VMID-699.nrp.co>
2022-01-27 20:03:14 +0000

    add commands for install zsh in install_zsh.sh

--- install_zsh.sh
+++ install_zsh.sh
@@ -1,2 +1,8 @@
 # inslall zsh
-#
+sudo apt-get install zsh
+sh -c "$(wget https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh -O -)"
+# set zsh default
+chsh -s $(which zsh)
+# add plugins
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+
